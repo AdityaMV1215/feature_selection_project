3864b938b8c5d5cc66f31c84f4c3ba8930c8fcf4
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-03-18 04:16:33 +0000

    Done

--- q01_plot_corr/build.py
+++ q01_plot_corr/build.py
@@ -1,5 +1,7 @@
 # Default imports
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 from matplotlib.pyplot import yticks, xticks, subplots, set_cmap
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
